c801fd71ab04a35d0b9dd01ffc894b96fc04fdf1
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-09-25 11:51:31 -0600

    make skeleton of general information inputs in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import GeneralInformation from "./components/general-information.jsx";
+
 function App() {
 
   return (
     <>
+      <h2>General Information</h2>
+      <GeneralInformation
+        labelText="Name"
+        type="text"
+      />
+      <GeneralInformation
+        labelText="Email"
+        type="email"
+      />
+      <GeneralInformation
+        labelText="Phone Number"
+        type="tel"
+      />
     </>
   )
 }
